348ba6d403a0bbd84fff165f4edb69aeaaa7a1a5
tshrpl <tshrpl@protonmail.com>
2022-05-29 02:27:55 +0530

    add tests for math library

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -3,9 +3,22 @@
 
 pub fn add_two_numbers<T>(a: T, b: T) -> T
 where
-    T: std::ops::Add<Output = T>
+	T: std::ops::Add<Output = T>
 {
-    a + b
+	a + b
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_add_two_numbers() {
+		assert_eq!(add_two_numbers(1_u8, 2_u8), 3_u8);
+		assert_eq!(add_two_numbers(2_i16, -3_i16), -1_i16);
+		assert_eq!(add_two_numbers(2_f64, -3_f64), -1_f64);
+	}
 }
 
 
